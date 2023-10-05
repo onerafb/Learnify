77b124f496b8597d6b85e591b5d0630bb11c9b2a
onerafb <aftab7t7@gmail.com>
2023-10-05 18:20:10 +0530

    Initial commit

--- server/app.js
+++ server/app.js
@@ -18,9 +18,9 @@ app.use(
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
+    origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
 );
 //Routes
